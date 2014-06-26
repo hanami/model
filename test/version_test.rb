27eef39e585805b56615665174c7890f4b47dba7
Luca Guidi <me@lucaguidi.com>
2014-06-26 18:42:56 +0200

    Bump version to 0.1.2

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Model::VERSION do
   it 'returns current version' do
-    Lotus::Model::VERSION.must_equal '0.1.1'
+    Lotus::Model::VERSION.must_equal '0.1.2'
   end
 end
