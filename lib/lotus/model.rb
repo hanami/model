85a0b6f6ee7174201ddb61dc8f8d8a54a984eff3
Trung Lê <trung.le@ruby-journal.com>
2014-07-13 12:49:59 +1000

    Initial work on Lotus::Model.adapter

--- lib/lotus/model.rb
+++ lib/lotus/model.rb
@@ -24,5 +24,63 @@ module Lotus
     # @see Lotus::Repository.update
     class NonPersistedEntityError < ::StandardError
     end
+
+    include Utils::ClassAttribute
+
+    Adapter = Struct.new(:name, :uri, :default)
+
+    # Framework adapters
+    #
+    # @since 0.2.0
+    # @api private
+    class_attribute :adapters
+    self.adapters = {}
+
+    # Configure the framework.
+    # It yields the given block in the context of the configuration
+    #
+    # @param blk [Proc] the configuration block
+    #
+    # @since 0.2.0
+    #
+    # @see Lotus::Model
+    #
+    # @example
+    #   require 'lotus/model'
+    #
+    #   Lotus::Model.configure do
+    #     adapter :sql, 'postgres://localhost/database', default: true
+    #   end
+    def self.configure(&block)
+      instance_eval(&block)
+    end
+
+    # Register adapter
+    #
+    # If `default` params is set to `true`, the adapter will be used as default one
+    #
+    # @param name    [Symbol] Derive adapter class name
+    # @param uri     [String] The adapter uri
+    # @param default [TrueClass,FalseClass] Decide if adapter is used by default
+    #
+    # @since 0.2.0
+    #
+    # @see Lotus::Model#adapter
+    # @see Lotus::Model#configure
+    #
+    # @example Register SQL Adapter as default adapter
+    #   require 'lotus/model'
+    #
+    #   Lotus::Model.adapters # => {}
+    #
+    # @example Register an adapter
+    #   require 'lotus/model'
+    #
+    #   Lotus::Model.configure do
+    #     adapter :sql, 'postgres://localhost/database', default: true
+    #   end
+    def self.adapter(name, uri, default: false)
+      adapters[name] = Adapter.new(name, uri, default)
+    end
   end
 end
---
+++ test/model_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+describe Lotus::Model do
+  describe '.configure' do
+    it 'evaluates the configuration block' do
+      -> do
+        Lotus::Model.configure do
+          raise 'the block has been evaluated'
+        end
+      end.must_raise RuntimeError
+    end
+  end
+
+  describe '.adapter' do
+    it 'allows to register SQL adapter' do
+      Lotus::Model.adapter :sql, 'postgres://localhost/database', default: true
+
+      adapter = Lotus::Model.adapters[:sql]
+
+      adapter.default.must_equal(true)
+      #adapter.class.must_equal(Lotus::Model::Adapters::SqlAdapter)
+      adapter.uri.must_equal('postgres://localhost/database')
+    end
+  end
+end
