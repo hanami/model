16fcc135fb5b154320d451b178aecf9601647dad
Trung Lê <trung.le@ruby-journal.com>
2014-07-29 23:11:32 +1000

    Add test for Configuration

---
+++ test/model/configuration_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+describe Lotus::Model::Configuration do
+  before do
+    @configuration = Lotus::Model::Configuration.new
+  end
+
+  describe '#adapters' do
+    it 'defaults to an empty set' do
+      @configuration.adapters.must_be_empty
+    end
+
+    it 'allows to register adapter' do
+      @configuration.adapter(:sql, 'postgres://localhost/database')
+
+      adapter = @configuration.adapters.fetch(:sql)
+      adapter.must_be_kind_of Lotus::Model::Config::Adapter
+      adapter.uri.must_equal 'postgres://localhost/database'
+    end
+
+    it 'allows to register default adapter' do
+      @configuration.adapter(:sql, 'postgres://localhost/database', default: true)
+
+      default_adapter = @configuration.adapters.fetch(:default)
+      default_adapter.must_be_kind_of Lotus::Model::Config::Adapter
+      default_adapter.uri.must_equal 'postgres://localhost/database'
+    end
+
+    it 'eliminates duplications' do
+      @configuration.adapter(:sql, 'postgres://localhost/database')
+      @configuration.adapter(:sql, 'postgres://localhost/database')
+
+      @configuration.adapters.size.must_equal(1)
+    end
+  end
+end
