52d5fe0fa953e5e5a5bead00028d77895ec5dde0
Trung Lê <trung.le@ruby-journal.com>
2014-07-30 23:44:22 +1000

    Refactor Configuration test

--- test/model/configuration_test.rb
+++ test/model/configuration_test.rb
@@ -1,36 +1,34 @@
 require 'test_helper'
 
 describe Lotus::Model::Configuration do
-  before do
-    @configuration = Lotus::Model::Configuration.new
-  end
+  let(:configuration) { Lotus::Model::Configuration.new }
 
   describe '#adapter' do
     it 'defaults to an empty set' do
-      @configuration.adapters.must_be_empty
+      configuration.adapters.must_be_empty
     end
 
     it 'allows to register adapter' do
-      @configuration.adapter(:sql, 'postgres://localhost/database')
+      configuration.adapter(:sql, 'postgres://localhost/database')
 
-      adapter = @configuration.adapters.fetch(:sql)
+      adapter = configuration.adapters.fetch(:sql)
       adapter.must_be_kind_of Lotus::Model::Config::Adapter
       adapter.uri.must_equal 'postgres://localhost/database'
     end
 
     it 'allows to register default adapter' do
-      @configuration.adapter(:sql, 'postgres://localhost/database', default: true)
+      configuration.adapter(:sql, 'postgres://localhost/database', default: true)
 
-      default_adapter = @configuration.adapters.fetch(:default)
+      default_adapter = configuration.adapters.fetch(:default)
       default_adapter.must_be_kind_of Lotus::Model::Config::Adapter
       default_adapter.uri.must_equal 'postgres://localhost/database'
     end
 
     it 'eliminates duplications' do
-      @configuration.adapter(:sql, 'postgres://localhost/database')
-      @configuration.adapter(:sql, 'postgres://localhost/database')
+      configuration.adapter(:sql, 'postgres://localhost/database')
+      configuration.adapter(:sql, 'postgres://localhost/database')
 
-      @configuration.adapters.size.must_equal(1)
+      configuration.adapters.size.must_equal(1)
     end
   end
 
