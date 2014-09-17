209b228d817381b1194edca4ade98466526217f9
Trung Lê <trung.le@ruby-journal.com>
2014-09-17 16:50:57 +1000

    Add integration test

---
+++ test/integration/configuration_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+describe 'Configuration DSL' do
+  before do
+    Lotus::Model.configure do
+      adapter :memory, 'memory://localhost'
+
+      mapping do
+        collection :users do
+          entity     User
+          repository UserRepository
+
+          attribute :id,   Integer
+          attribute :name, String
+        end
+      end
+    end
+
+    Lotus::Model.load!
+  end
+
+  after do
+    Lotus::Model.unload!
+  end
+
+  describe 'when creating new user' do
+    before do
+      @user = User.new(name: 'Trung')
+    end
+
+    it 'add the entity to repositories' do
+      @user_counter = UserRepository.all.size
+
+      UserRepository.create(@user)
+
+      users = UserRepository.all
+      users.size.must_equal(@user_counter + 1)
+      users.first.must_equal(@user)
+    end
+  end
+end
