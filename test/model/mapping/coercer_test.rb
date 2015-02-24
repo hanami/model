84ddb21601e55f33aca979d5a702a56cf6664714
Luca Guidi <me@lucaguidi.com>
2015-02-24 11:38:06 +0100

    When creating a record, don't send nil values to the database. Closes #155

--- test/model/mapping/coercer_test.rb
+++ test/model/mapping/coercer_test.rb
@@ -9,6 +9,7 @@ describe Lotus::Model::Mapping::Coercer do
     collection.entity(User)
     collection.attribute :id,   Integer
     collection.attribute :name, String
+    collection.attribute :age,  String
   end
 
   describe '#to_record' do
@@ -16,11 +17,23 @@ describe Lotus::Model::Mapping::Coercer do
       coercer.to_record(entity).must_equal(name: 'Tyrion Lannister')
     end
 
+    # Bug:
+    # https://github.com/lotus/model/issues/155
+    it 'should not set keys for nil values when creating' do
+      entity = User.new(name: 'Daenerys Targaryen', age: nil)
+      coercer.to_record(entity).must_equal(name: 'Daenerys Targaryen')
+    end
+
+    it 'should set keys for nil values when updating' do
+      entity = User.new(id: 4, name: 'Daenerys Targaryen', age: nil)
+      coercer.to_record(entity).must_equal(id: 4, name: 'Daenerys Targaryen', age: nil)
+    end
+
     describe 'when identity is set' do
       let(:entity) { User.new(id: 3, name: 'Tyrion Lannister') }
 
       it 'should return identity as well' do
-        coercer.to_record(entity).must_equal(id: 3, name: 'Tyrion Lannister')
+        coercer.to_record(entity).must_equal(id: 3, name: 'Tyrion Lannister', age: nil)
       end
     end
   end
