3a3e6fd628373ab81d25b92f23d32b01b03870fa
Alfonso Uceda Pompa <alfonso@aspgems.com>
2015-03-11 10:37:11 +0100

    Fix send nil value when creating an entity

--- test/model/mapping/coercer_test.rb
+++ test/model/mapping/coercer_test.rb
@@ -9,6 +9,7 @@ describe Lotus::Model::Mapping::Coercer do
     collection.entity(User)
     collection.attribute :id,   Integer
     collection.attribute :name, String
+    collection.attribute :age, String
   end
 
   describe '#to_record' do
@@ -16,22 +17,41 @@ describe Lotus::Model::Mapping::Coercer do
       coercer.to_record(entity).must_equal(name: 'Tyrion Lannister')
     end
 
+    describe 'new record' do
+      # Bug:
+      # https://github.com/lotus/model/issues/155
+      it 'ignores unset values' do
+        entity = User.new(name: 'Daenerys Targaryen')
+        coercer.to_record(entity).must_equal(name: 'Daenerys Targaryen')
+      end
+
+      it 'forces nil values' do
+        entity = User.new(name: 'Daenerys Targaryen', age: nil)
+        coercer.to_record(entity).must_equal(name: 'Daenerys Targaryen')
+      end
+    end
+
+    it 'should set keys for nil values when updating' do
+      entity = User.new(id: 4, name: 'Daenerys Targaryen', age: nil)
+      coercer.to_record(entity).must_equal(id: 4, name: 'Daenerys Targaryen', age: nil)
+    end
+
     describe 'when identity is set' do
       let(:entity) { User.new(id: 3, name: 'Tyrion Lannister') }
 
       it 'should return identity as well' do
-        coercer.to_record(entity).must_equal(id: 3, name: 'Tyrion Lannister')
+        coercer.to_record(entity).must_equal(id: 3, name: 'Tyrion Lannister', age: nil)
       end
     end
   end
-  
+
   describe '#from_record' do
     before do
       collection.entity(::Repository)
       collection.attribute :id,   Integer
       collection.attribute :name, String
     end
-    
+
     it 'should use the correct entity class' do
       coercer.from_record(name: 'production').class.must_equal(::Repository)
     end
