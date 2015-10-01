166526b3624e46985fc99dbcf325ca7210620e24
Helio Costa e Silva <helio@bankfacil.com.br>
2015-10-01 15:15:32 -0300

    Added Association Relationships API docs

--- lib/lotus/model/associations/one_to_many.rb
+++ lib/lotus/model/associations/one_to_many.rb
@@ -1,10 +1,24 @@
 module Lotus
   module Model
     module Associations
+      # Entities Association OneToMany relationship
       class OneToMany
+        # @attr_reader related collection (the other side of relationship)
+        #
+        # @since x.x.x
         attr_reader :collection
+
+        # @attr_reader Repository responsible will roots this association
+        #
+        # @since x.x.x
         attr_accessor :repository
 
+        # Collects initial data to build ManyToOne Association
+        # @example
+        #
+        #   collection: :articles, foreign_key: :article_id, repository: ArticlesRepository
+        #
+        # @since x.x.x
         def initialize(opts)
           @name        = opts.fetch(:name)
           @collection  = opts.fetch(:collection)
@@ -12,6 +26,8 @@ module Lotus
           @repository  = nil
         end
 
+        # @since x.x.x
+        # @api private
         def associate_entities!(entities)
           entities.map do |entity|
             entity.send("#{@name}=", by_foreign_key(entity.id))
@@ -20,7 +36,8 @@ module Lotus
         end
 
         private
-        
+        # Fetch Relation by its foreign key
+        # @api private
         def by_foreign_key(key)
           @repository.send(:query).where(@foreign_key => key).to_a
         end
