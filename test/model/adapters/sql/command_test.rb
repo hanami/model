0e150f39b92c67e322e9cbb01bd257168c523429
Trung Lê <trung.le@ruby-journal.com>
2016-01-19 19:45:35 +1100

    Add test to assert exception message

--- test/model/adapters/sql/command_test.rb
+++ test/model/adapters/sql/command_test.rb
@@ -14,61 +14,107 @@ describe Lotus::Model::Adapters::Sql::Command do
   end
 
   describe '#create' do
-    describe 'when a Sequel::DatabaseError is raised' do
-      it 'raises a lotus error' do
+    describe 'when a Sequel::ForeignKeyConstraintViolation is raised' do
+      it 'raises Lotus::Model::Error exception' do
         collection.define_singleton_method(:insert) do |_|
-          raise Sequel::DatabaseError.new
+          raise ::Sequel::ForeignKeyConstraintViolation.new('fkey constraint error')
         end
-        -> { @command.create(Object.new) }.must_raise(Lotus::Model::Error)
+        exception = -> { @command.create(Object.new) }.must_raise(Lotus::Model::Error)
+        exception.message.must_equal('fkey constraint error')
       end
     end
 
-    describe 'when a different error is raised' do
+    describe 'when a Sequel::CheckConstraintViolation is raised' do
+      it 'raises Lotus::Model::Error exception' do
+        collection.define_singleton_method(:insert) do |_|
+          raise ::Sequel::CheckConstraintViolation.new('check constraint error')
+        end
+        exception = -> { @command.create(Object.new) }.must_raise(Lotus::Model::Error)
+        exception.message.must_equal('check constraint error')
+      end
+    end
+
+    describe 'when a Sequel::NotNullConstraintViolation is raised' do
+      it 'raises Lotus::Model::Error exception' do
+        collection.define_singleton_method(:insert) do |_|
+          raise ::Sequel::NotNullConstraintViolation.new('not null constraint error')
+        end
+        exception = -> { @command.create(Object.new) }.must_raise(Lotus::Model::Error)
+        exception.message.must_equal('not null constraint error')
+      end
+    end
+
+    describe 'when a Sequel::UniqueConstraintViolation is raised' do
+      it 'raises Lotus::Model::Error exception' do
+        collection.define_singleton_method(:insert) do |_|
+          raise ::Sequel::UniqueConstraintViolation.new('unique constraint error')
+        end
+        exception = -> { @command.create(Object.new) }.must_raise(Lotus::Model::Error)
+        exception.message.must_equal('unique constraint error')
+      end
+    end
+
+    describe 'when a Sequel::DatabaseError is raised' do
+      it 'raises Lotus::Model::Error exception' do
+        collection.define_singleton_method(:insert) do |_|
+          raise ::Sequel::DatabaseError.new('db error')
+        end
+        exception = -> { @command.create(Object.new) }.must_raise(Lotus::Model::Error)
+        exception.message.must_equal('db error')
+      end
+    end
+
+    describe 'when an error that is not inherited from Sequel::DatabaseError is raised' do
       it 'bubbles the error up' do
         collection.define_singleton_method(:insert) do |_|
-          raise Sequel::Error.new
+          raise Sequel::Error.new('constraint error')
         end
-        -> { @command.create(Object.new) }.must_raise(Sequel::Error)
+        exception = -> { @command.create(Object.new) }.must_raise(Sequel::Error)
+        exception.message.must_equal('constraint error')
       end
     end
   end
 
   describe '#update' do
     describe 'when a Sequel::DatabaseError is raised' do
-      it 'raises a lotus error' do
+      it 'raises Lotus::Model::Error exception' do
         collection.define_singleton_method(:update) do |_|
-          raise Sequel::DatabaseError.new
+          raise Sequel::DatabaseError.new('db error')
         end
-        -> { @command.update(Object.new) }.must_raise(Lotus::Model::Error)
+        exception = -> { @command.update(Object.new) }.must_raise(Lotus::Model::Error)
+        exception.message.must_equal('db error')
       end
     end
 
     describe 'when a different error is raised' do
       it 'bubbles the error up' do
         collection.define_singleton_method(:update) do |_|
-          raise Sequel::Error.new
+          raise Sequel::Error.new('constraint error')
         end
-        -> { @command.update(Object.new) }.must_raise(Sequel::Error)
+        exception = -> { @command.update(Object.new) }.must_raise(Sequel::Error)
+        exception.message.must_equal('constraint error')
       end
     end
   end
 
   describe '#delete' do
     describe 'when a Sequel::DatabaseError is raised' do
-      it 'raises a lotus error' do
+      it 'raises Lotus::Model::Error exception' do
         collection.define_singleton_method(:delete) do
-          raise Sequel::DatabaseError.new
+          raise Sequel::DatabaseError.new('db error')
         end
-        -> { @command.delete }.must_raise(Lotus::Model::Error)
+        exception = -> { @command.delete }.must_raise(Lotus::Model::Error)
+        exception.message.must_equal('db error')
       end
     end
 
     describe 'when a different error is raised' do
       it 'bubbles the error up' do
         collection.define_singleton_method(:delete) do
-          raise Sequel::Error.new
+          raise Sequel::Error.new('constraint error')
         end
-        -> { @command.delete }.must_raise(Sequel::Error)
+        exception = -> { @command.delete }.must_raise(Sequel::Error)
+        exception.message.must_equal('constraint error')
       end
     end
   end
