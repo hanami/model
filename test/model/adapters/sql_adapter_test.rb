hanami/model

commit e6e425ff56dfc7865e3bc86332dfe413b3c6f36c

Cleanup for #308
  * Specify adapter name for `MissingURIError`
  * Adapter URI are always mandatory
  * Fix tests: assert adapter uri with nil and blank will raise `MissingURIError`
  * Solid "blankness" uri check with Utils::Blank

lib/hanami/model/adapters/abstract.rb
@@ -1,4 +1,6 @@
 require 'hanami/utils/basic_object'
+require 'hanami/utils/string'
+require 'hanami/utils/blank'
 
 module Hanami
   module Model
@@ -29,8 +31,8 @@ module Hanami
       #
       # @since x.x.x
       class MissingURIError < Hanami::Model::Error
-        def initialize
-          super "URI for FileSystemAdapter is nil or empty"
+        def initialize(adapter_name)
+          super "URI for `#{ adapter_name }' adapter is nil or empty. Please check env variables like `DATABASE_URL'."
         end
       end
 
@@ -102,7 +104,7 @@ module Hanami
           @uri    = uri
           @options = options
 
-          assert_uri_present! if uri_mandatory?
+          assert_uri_present!
         end
 
         # Creates or updates a record in the database for the given entity.
@@ -287,17 +289,19 @@ module Hanami
           raise NotImplementedError
         end
 
+        # Adapter name
+        #
+        # @return [String] adapter name
+        #
+        # @since x.x.x
+        def adapter_name
+          Utils::String.new(self.class.name).demodulize.underscore.to_s
+        end
+
         private
 
         def assert_uri_present!
-          raise MissingURIError if @uri.nil? || @uri.empty?
-        end
-
-        # Lets subclasses decide if they need a URI.
-        #
-        # @since x.x.x
-        def uri_mandatory?
-          true
+          raise MissingURIError.new(adapter_name) if Utils::Blank.blank?(@uri)
         end
       end
     end

lib/hanami/model/adapters/file_system_adapter.rb
@@ -282,18 +282,6 @@ module Hanami
         def _load(contents)
           Marshal.load(contents)
         end
-
-        private
-
-
-        # MemoryAddapter (which this Adapter inherits from) does not need a uri
-        # but this adapter does (to know where to put the files).
-        #
-        # @api private
-        # @since x.x.x
-        def uri_mandatory?
-          true
-        end
       end
     end
   end

lib/hanami/model/adapters/memory_adapter.rb
@@ -173,14 +173,6 @@ module Hanami
         def synchronize
           @mutex.synchronize { yield }
         end
-
-        # Most adapters need a URI, but this one doesn't
-        #
-        # @api private
-        # @since x.x.x
-        def uri_mandatory?
-          false
-        end
       end
     end
   end

test/model/adapters/abstract_test.rb
@@ -85,19 +85,29 @@ describe Hanami::Model::Adapters::Abstract do
     end
   end
 
-  describe 'empty path' do
+  describe '#adapter_name' do
+    it 'returns demodulized, underscored class name' do
+      adapter.adapter_name.must_equal 'abstract'
+    end
+  end
+
+  describe 'empty uri' do
     it 'raises MissingURIError' do
-      -> {
+      exception = -> {
         Hanami::Model::Adapters::Abstract.new(@mapper, nil)
       }.must_raise(Hanami::Model::Adapters::MissingURIError)
+
+      exception.message.must_equal "URI for `abstract' adapter is nil or empty. Please check env variables like `DATABASE_URL'."
     end
   end
 
   describe 'nil path' do
     it 'raises MissingURIError' do
-      -> {
-        Hanami::Model::Adapters::Abstract.new(@mapper, nil)
+      exception = -> {
+        Hanami::Model::Adapters::Abstract.new(@mapper, "")
       }.must_raise(Hanami::Model::Adapters::MissingURIError)
+
+      exception.message.must_equal "URI for `abstract' adapter is nil or empty. Please check env variables like `DATABASE_URL'."
     end
   end
 end

commit f952fa45b3c1c9dfdeda42e05bda26258ea9c565

Fix build errors introduced by e6e425f

lib/hanami/model/adapters/abstract.rb
@@ -89,6 +89,12 @@ module Hanami
       #
       # @since 0.1.0
       class Abstract
+        # @since x.x.x
+        # @api private
+        #
+        # @see Hanami::Model::Adapters::Abstract#adapter_name
+        ADAPTER_NAME_SUFFIX = '_adapter'.freeze
+
         # Initialize the adapter
         #
         # @param mapper [Hanami::Model::Mapper] the object that defines the
@@ -295,7 +301,7 @@ module Hanami
         #
         # @since x.x.x
         def adapter_name
-          Utils::String.new(self.class.name).demodulize.underscore.to_s
+          Utils::String.new(self.class.name).demodulize.underscore.to_s.sub(ADAPTER_NAME_SUFFIX, '')
         end
 
         private

test/integration/repository_test.rb
@@ -10,7 +10,7 @@ describe Hanami::Repository do
   let(:article3) { Article.new(user_id: user2.id, title: 'Love Relationships',       comments_count: '4') }
 
   {
-    memory:      [Hanami::Model::Adapters::MemoryAdapter,     nil,                           MAPPER],
+    memory:      [Hanami::Model::Adapters::MemoryAdapter,     MEMORY_CONNECTION_STRING,      MAPPER],
     file_system: [Hanami::Model::Adapters::FileSystemAdapter, FILE_SYSTEM_CONNECTION_STRING, MAPPER],
     sqlite:      [Hanami::Model::Adapters::SqlAdapter,        SQLITE_CONNECTION_STRING,      MAPPER],
     postgres:    [Hanami::Model::Adapters::SqlAdapter,        POSTGRES_CONNECTION_STRING,    MAPPER],
@@ -611,8 +611,8 @@ describe Hanami::Repository do
 
   describe "with memory adapter" do
     before do
-      UserRepository.adapter    = Hanami::Model::Adapters::MemoryAdapter.new(MAPPER, nil)
-      ArticleRepository.adapter = Hanami::Model::Adapters::MemoryAdapter.new(MAPPER, nil)
+      UserRepository.adapter    = Hanami::Model::Adapters::MemoryAdapter.new(MAPPER, MEMORY_CONNECTION_STRING)
+      ArticleRepository.adapter = Hanami::Model::Adapters::MemoryAdapter.new(MAPPER, MEMORY_CONNECTION_STRING)
 
       UserRepository.collection    = :users
       ArticleRepository.collection = :articles

test/model/adapters/file_system_adapter_test.rb
@@ -1175,5 +1175,11 @@ describe Hanami::Model::Adapters::FileSystemAdapter do
       exception = -> { @adapter.create(collection, TestUser.new) }.must_raise Hanami::Model::Adapters::DisconnectedAdapterError
       exception.message.must_match "You have tried to perform an operation on a disconnected adapter"
     end
+
+    describe '#adapter_name' do
+      it "equals to 'file_system'" do
+        @adapter.adapter_name.must_equal 'file_system'
+      end
+    end
   end
 end

test/model/adapters/memory_adapter_test.rb
@@ -34,7 +34,7 @@ describe Hanami::Model::Adapters::MemoryAdapter do
       end
     end.load!
 
-    @adapter = Hanami::Model::Adapters::MemoryAdapter.new(@mapper)
+    @adapter = Hanami::Model::Adapters::MemoryAdapter.new(@mapper, MEMORY_CONNECTION_STRING)
   end
 
   after do
@@ -1216,6 +1216,12 @@ describe Hanami::Model::Adapters::MemoryAdapter do
       it 'raises error' do
         exception = -> { @adapter.create(collection, TestUser.new) }.must_raise Hanami::Model::Adapters::DisconnectedAdapterError
         exception.message.must_match "You have tried to perform an operation on a disconnected adapter"
+      end
+    end
+
+    describe '#adapter_name' do
+      it "equals to 'memory'" do
+        @adapter.adapter_name.must_equal 'memory'
       end
     end
   end

test/model/adapters/sql_adapter_test.rb
@@ -1392,5 +1392,11 @@ describe Hanami::Model::Adapters::SqlAdapter do
         exception.message.must_match "You have tried to perform an operation on a disconnected adapter"
       end
     end
+
+    describe '#adapter_name' do
+      it "equals to 'sql'" do
+        @adapter.adapter_name.must_equal 'sql'
+      end
+    end
   end
 end

test/test_helper.rb
@@ -50,6 +50,7 @@ else
   POSTGRES_CONNECTION_STRING = "postgres://localhost/#{ postgres_database }"
 end
 
+MEMORY_CONNECTION_STRING      = "memory://test"
 FILE_SYSTEM_CONNECTION_STRING = "file:///#{ filesystem }"
 
 if ENV['TRAVIS'] == 'true'