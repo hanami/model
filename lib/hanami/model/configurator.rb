1301fd2ebf1159ea9011bbd9c4989b3ae7346558
Luca Guidi <me@lucaguidi.com>
2017-01-13 20:19:13 +0100

    DRY Configurator with only migrations_logger. Ref #368

--- lib/hanami/model/configurator.rb
+++ lib/hanami/model/configurator.rb
@@ -29,14 +29,17 @@ module Hanami
       # @api private
       attr_reader :_logger
 
-      # @since x.x.x
-      # @api private
-      attr_reader :_migrations_logger
-
       # @since 0.7.0
       # @api private
       def self.build(&block)
         new.tap { |config| config.instance_eval(&block) }
+      end
+
+      # @since x.x.x
+      # @api private
+      def migrations_logger(stream = $stdout)
+        require 'hanami/model/migrator/logger'
+        @migrations_logger ||= Hanami::Model::Migrator::Logger.new(stream)
       end
 
       private
@@ -73,13 +76,6 @@ module Hanami
 
         opts = options.merge(stream: stream)
         @_logger = Hanami::Logger.new('hanami.model', opts)
-      end
-
-      # @since x.x.x
-      # @api private
-      def migrations_logger(stream = $stdout)
-        require 'hanami/model/migrator/logger'
-        @_migrations_logger = Hanami::Model::Migrator::Logger.new(stream)
       end
     end
   end
