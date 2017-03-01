hanami/model

commit 0a3768e7e2308e1fd4d67d85dcd0bad9bffe0e4d

Cleanup for #384

lib/hanami/model/migrator/connection.rb
@@ -44,7 +44,7 @@ module Hanami
         # @since 0.5.0
         # @api private
         def port
-          @port ||= parsed_uri.port || parsed_opt('port').to_i.nonzero?
+          @port ||= parsed_uri.port || Integer(parsed_opt('port'))
         end
 
         # Returns DB name from conenction

test/migrator/connection_test.rb
@@ -79,6 +79,7 @@ describe Hanami::Model::Migrator::Connection do
       end
     end
 
+    # See https://www.postgresql.org/docs/current/static/libpq-connect.html#LIBPQ-CONNSTRING
     describe 'when connection components in uri params' do
       let(:hanami_model_configuration) do
         OpenStruct.new(
@@ -104,6 +105,18 @@ describe Hanami::Model::Migrator::Connection do
 
       it 'returns configured port' do
         connection.port.must_equal 6433
+      end
+
+      describe 'with blank port' do
+        let(:hanami_model_configuration) do
+          OpenStruct.new(
+            url: 'postgresql:///mydb?host=localhost&port=&user=postgres&password=testpasswd'
+          )
+        end
+
+        it 'raises an error' do
+          -> { connection.port }.must_raise TypeError
+        end
       end
     end
   end

commit bae750c865523fbdd5b8f3622172745f5e1ac1ae

Fix spec problem introduced by 0a3768e

lib/hanami/model/migrator/connection.rb
@@ -44,7 +44,7 @@ module Hanami
         # @since 0.5.0
         # @api private
         def port
-          @port ||= parsed_uri.port || Integer(parsed_opt('port'))
+          @port ||= parsed_uri.port || parsed_opt('port').to_i.nonzero?
         end
 
         # Returns DB name from conenction

test/migrator/connection_test.rb
@@ -115,7 +115,7 @@ describe Hanami::Model::Migrator::Connection do
         end
 
         it 'raises an error' do
-          -> { connection.port }.must_raise TypeError
+          connection.port.must_equal nil
         end
       end
     end