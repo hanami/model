c9dc079b2c6bf1abfac39c3aa25545b318e06bd2
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-09-24 11:40:48 +0300

    [tmp] silence ruby warnings in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   config.filter_run_when_matching :focus
   config.disable_monkey_patching!
 
-  config.warnings = true
+  # config.warnings = true
 
   config.default_formatter = 'doc' if config.files_to_run.one?
 
