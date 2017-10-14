9d4b3c2cee0cf095b15f313200db4e15f1e42ca0
Luca Guidi <me@lucaguidi.com>
2017-10-14 15:29:52 +0200

    Prepare for v1.0.4

--- lib/hanami/model/version.rb
+++ lib/hanami/model/version.rb
@@ -3,6 +3,6 @@ module Hanami
     # Defines the version
     #
     # @since 0.1.0
-    VERSION = '1.0.3'.freeze
+    VERSION = '1.0.4'.freeze
   end
 end
--- spec/unit/hanami/model/version_spec.rb
+++ spec/unit/hanami/model/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Model::VERSION" do
   it 'exposes version' do
-    expect(Hanami::Model::VERSION).to eq('1.0.3')
+    expect(Hanami::Model::VERSION).to eq('1.0.4')
   end
 end
