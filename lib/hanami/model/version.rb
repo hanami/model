b78c1d44f59489a31920faa55b572fb66820d161
Luca Guidi <me@lucaguidi.com>
2017-11-29 18:15:50 +0100

    Depend on hanami-utils 2.0.0.alpha1

--- lib/hanami/model/version.rb
+++ lib/hanami/model/version.rb
@@ -5,6 +5,6 @@ module Hanami
     # Defines the version
     #
     # @since 0.1.0
-    VERSION = "1.1.0"
+    VERSION = "2.0.0.alpha1"
   end
 end
--- spec/unit/hanami/model/version_spec.rb
+++ spec/unit/hanami/model/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Model::VERSION" do
   it "exposes version" do
-    expect(Hanami::Model::VERSION).to eq("1.1.0")
+    expect(Hanami::Model::VERSION).to eq("2.0.0.alpha1")
   end
 end
