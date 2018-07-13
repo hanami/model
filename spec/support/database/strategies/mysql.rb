1822ccdb611ceff77eeb847bd1039c342dc6a681
Luca Guidi <me@lucaguidi.com>
2018-07-13 11:46:23 +0200

    Differentiate CI strategy for MySQL

--- spec/support/database/strategies/mysql.rb
+++ spec/support/database/strategies/mysql.rb
@@ -31,22 +31,41 @@ module Database
         end
       end
 
-      module CiImplementation
+      module TravisCiImplementation
         protected
 
         def export_env
           super
-          ENV['HANAMI_DATABASE_USERNAME']   = 'travis' if ENV['TRAVIS']
-          ENV['HANAMI_DATABASE_USERNAME'] ||= 'root'   if ENV['CIRCLECI']
+          ENV['HANAMI_DATABASE_USERNAME'] = 'travis'
           ENV['HANAMI_DATABASE_URL'] = "mysql2://#{credentials}@#{host}/#{database_name}"
         end
 
         def create_database
           super
-          return if root?
-
           run_command "GRANT ALL PRIVILEGES ON *.* TO '#{ENV['HANAMI_DATABASE_USERNAME']}'@'#{host}'; FLUSH PRIVILEGES;"
           run_command "GRANT ALL PRIVILEGES ON *.* TO '#{ENV['HANAMI_DATABASE_USERNAME']}'@'%'; FLUSH PRIVILEGES;" if jruby?
+        end
+
+        private
+
+        def run_command(command)
+          result = system %(mysql -u root -e "#{command}")
+          raise "Failed command:\n#{command}" unless result
+        end
+      end
+
+      module CircleCiImplementation
+        protected
+
+        def export_env
+          super
+          ENV['HANAMI_DATABASE_USERNAME'] ||= 'root'
+          ENV['HANAMI_DATABASE_URL'] = "mysql2://#{credentials}@#{host}/#{database_name}"
+        end
+
+        def create_database
+          run_command "DROP DATABASE IF EXISTS #{database_name}"
+          run_command "CREATE DATABASE #{database_name}"
         end
 
         private
@@ -62,7 +81,13 @@ module Database
       end
 
       def initialize
-        extend(CiImplementation)    if ci?
+        ci_implementation = Platform.match do
+          ci(:travis) { TravisCiImplementation }
+          ci(:circle) { CircleCIImplementation }
+          default { Module.new }
+        end
+
+        extend(ci_implementation)
         extend(JrubyImplementation) if jruby?
       end
 
@@ -84,17 +109,13 @@ module Database
       def create_database
         run_command "DROP DATABASE IF EXISTS #{database_name}"
         run_command "CREATE DATABASE #{database_name}"
-        run_command "GRANT ALL PRIVILEGES ON #{database_name}.* TO '#{ENV['HANAMI_DATABASE_USERNAME']}'@'#{host}'; FLUSH PRIVILEGES;" unless root?
+        run_command "GRANT ALL PRIVILEGES ON #{database_name}.* TO '#{ENV['HANAMI_DATABASE_USERNAME']}'@'#{host}'; FLUSH PRIVILEGES;"
       end
 
       private
 
       def run_command(command)
         system %(mysql -h #{host} -u #{ENV['HANAMI_DATABASE_USERNAME']} --password=#{ENV['HANAMI_DATABASE_PASSWORD']} -e "#{command}")
-      end
-
-      def root?
-        ENV['HANAMI_DATABASE_USERNAME'] == 'root'
       end
     end
   end
