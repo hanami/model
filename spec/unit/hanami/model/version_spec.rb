1ecfb99cc0c057a6b2fdb5270186ce4e109f7773
Luca Guidi <me@lucaguidi.com>
2019-01-31 13:43:45 +0100

    Depend on `dry-logic` `~> 0.4.2`, `< 0.5` (#515)

--- spec/unit/hanami/model/version_spec.rb
+++ spec/unit/hanami/model/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Model::VERSION" do
   it "exposes version" do
-    expect(Hanami::Model::VERSION).to eq("1.3.1")
+    expect(Hanami::Model::VERSION).to eq("1.3.2")
   end
 end
